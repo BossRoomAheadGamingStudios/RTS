99657ab6c9f791ea04270b40060afe0baf10c6dd
daviematz <daviematz@gmail.com>
2017-01-06 10:44:19 -0500

    Made a simple scene with rocks, water, an enemy, and a human worker. Messed around with navigation so the human worker follows the Enemy. Human worker flips depending on the x direction it is moving on.
    TODO: 
    Make human worker go to a location you provide by right clicking.
    Make human worker run a walk animation once it is provided by Tevin

---
+++ New Unity Project/Assets/Standard Assets/2D/Scripts/My Scripts/BasicNavigation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicNavigation : MonoBehaviour
+{
+
+    public Transform victim;
+    public bool facingRight = false;
+
+    private NavMeshAgent navComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        navComponent = GetComponent<NavMeshAgent>();
+        navComponent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victim)
+        {
+            navComponent.SetDestination(victim.position);
+            float h = navComponent.velocity.x;
+
+            if (h > 0 && !facingRight)
+                Flip();
+            else if (h < 0 && facingRight)
+                Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
